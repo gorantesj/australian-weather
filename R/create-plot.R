69d84e95284e434cff6c29597eccb69442d10f23
gorantesj <gorantesj@hotmail.com>
2021-08-24 14:57:52 +0100

    Se hizo un nuevo chart

--- R/create-plot.R
+++ R/create-plot.R
@@ -19,3 +19,13 @@ df %>%
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
+
+
+# Nueva grafica
+time_series =
+  df %>%
+  filter(Location == city) %>%
+  ggplot(aes(x = Date, y = Temp9am)) +
+  geom_line()
+ggsave(filename = "graphics/timeseries-rainfall.png",
+       plot = time_series)
